98d9392d03d3024decda485ffbbaf8862e124191
James <james@terra.money>
2021-09-10 11:59:41 +0900

    add string plus

--- src/scripts.js
+++ src/scripts.js
@@ -2,3 +2,4 @@ export const plus = (a, b) => a + b;
 export const div = (a, b) => a / b;
 export const multiple = (a, b) => a * b;
 export const minus = (a, b) => a - b;
+export const stringPlus = (str1, str2) => `${str1}${str2}`;
