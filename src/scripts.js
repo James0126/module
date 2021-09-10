434f525e7fd18c5043ca1c8bc072c35f9db73bbe
James <james@terra.money>
2021-09-10 12:22:48 +0900

    fix typo

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,5 @@
 export const plus = (a, b) => a + b;
 export const div = (a, b) => a / b;
-export const multiple = (a, b) => a * b;
+export const multiply = (a, b) => a * b;
 export const minus = (a, b) => a - b;
 export const stringPlus = (str1, str2) => `${str1}${str2}`;
